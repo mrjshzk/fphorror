4ffae385b079b50aaecae88d6e3596e6793d317d
Miguel Silva <115943663+mrjshzk@users.noreply.github.com>
2024-06-12 17:43:00 +0100

    nah, I'd rotate

--- scenes/player/door_raycast.gd
+++ scenes/player/door_raycast.gd
@@ -14,7 +14,6 @@ func _unhandled_input(event)-> void:
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("interact"):
 		if self.is_colliding():
-			print("Door loading")
 			GlobalSignals.lock_camera.emit()
 			is_holding = true
 			object = self.get_collider()
@@ -25,23 +24,21 @@ func _input(event: InputEvent) -> void:
 
 func _physics_process(delta: float) -> void:
 	if is_holding:
-		if object != null and object == self.get_collider():
-			print(object.get_parent_node_3d().get_node("%Joint").rotation * get_speed_add())
-			object.apply_force(object.get_parent_node_3d().get_node("%Joint").global_position * get_speed_add())
+		if object != null:
+			print(Vector3(0,1,0) * -get_speed_add() * 20)
+			object.apply_torque(Vector3(0,1,0) * -get_speed_add() * 20)
 
 func _process(delta: float) -> void:
 	mouse_input = Vector2.ZERO
 
-# TODO
+# KEEPINMIND
 func get_speed_add() -> float:
 	var vBodyCenter := self.transform * object.center_of_mass
 	var jointToBody := (vBodyCenter - object.get_parent_node_3d().global_position).normalized()
 	var vUp := self.transform.basis.y
 	var vRight := self.transform.basis.x
 	var vForward := -self.transform.basis.z
-	
 	var push_amount = (vUp + vForward) * mouse_input.y + vRight * -mouse_input.x
-	
 	var vPushRotateDir = jointToBody.cross(push_amount)
 	return vPushRotateDir.dot(Vector3(0,1,0))
 	
