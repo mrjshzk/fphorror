0a114daabd05cf49af84dbda8e5d8eafb6e69f7e
Miguel Silva <115943663+mrjshzk@users.noreply.github.com>
2024-06-13 22:13:18 +0100

    more work

--- scenes/player/door_raycast.gd
+++ scenes/player/door_raycast.gd
@@ -25,17 +25,28 @@ func _input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	if is_holding:
 		if object != null:
-			var force_dir := object.global_position.direction_to(global_position)
-			var vUp := self.global_transform.basis.y
-			var vRight := self.global_transform.basis.x
-			var vForward := -self.global_transform.basis.z
-			var push_amount = (vUp + vForward) * mouse_input.y + vRight * -mouse_input.x
-			var push :Vector3= Vector3.UP * push_amount * signf(force_dir.dot(-object.global_basis.z)) * 20
-			print(push)
-			object.apply_torque(push)
+			var vRotateDir := Vector3.UP
+			var vBodyVel := object.angular_velocity
+			var hingeVel = vRotateDir * vRotateDir.dot(vBodyVel)
+			print(hingeVel)
+			object.apply_torque(Vector3.UP * get_speed_add() * 30)
+			
 
 
 func _process(delta: float) -> void:
 	mouse_input = Vector2.ZERO
 
+	
+func get_speed_add() -> float:
+	var camera : Camera3D = get_parent_node_3d()
+	var camera_trans := camera.get_camera_transform()
+	var vBodyCenter := object.transform * object.center_of_mass
+	var vJointToBody := (vBodyCenter - object.global_position).normalized()
+	var vUp := camera_trans.basis.y
+	var vRight := camera.basis.x
+	var vForward := -camera_trans.basis.z
+	var vPushAmount := (vUp + vForward) * mouse_input.y + vRight * -mouse_input.x
+	var vPushRotateDir := vJointToBody.cross(vPushAmount)
+	
+	return vPushRotateDir.dot(Vector3.UP)
 	
